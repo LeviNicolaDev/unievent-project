27cebee586ab06b352c56292cd510902caa0443c
LeviNicolaDev <levinicolas2000@gmail.com>
2025-11-17 17:37:13 -0300

    [feat] altera a conexão com banco de dados

--- Config/Connection.php
+++ Config/Connection.php
@@ -1,32 +1,32 @@
 <?php
-  namespace src\Config;
-    use PDO;
-    use PDOException;
+namespace src\Config;
+    use PDO;
+    use PDOException;
 
-    class Connection{
-        private $server = "localhost";
-        private $username= "root";
-        private $db_name= "bdunievent";
-        private $password="";
-        private $port='3307';
-        public $conn;
+    class Connection{
+        private $server = $_ENV['DB_HOST'];
+        private $username= $_ENV['DB_USER'];
+        private $db_name= $_ENV['DB_NAME'];
+        private $password= $_ENV['DB_PASSWORD'];
+        private $port= $_ENV['DB_PORT'];
 
-        function __construct(){
-            $this->getConnection();
-        }
+        public $conn;
 
-        public function getConnection(){
-            try{
-                $this->conn=new PDO("mysql:host=$this->server:$this->port;dbname=$this->db_name",$this->username,$this->password);
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION );
-                $this->conn->exec('set names utf8');
-                 return $this->conn;
-            }
-            catch(PDOException $e){
-               die( "ERROR: ".$e->getMessage());
-            }
-           
-        }
-        
-    }
+        function __construct(){
+            $this->getConnection();
+        }
+
+        public function getConnection(){
+            try{
+                $dsn = "mysql:host=$this->server;port=$this->port;dbname=$this->db_name";
+                $this->conn=new PDO($dsn, $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION );
+                $this->conn->exec('set names utf8');
+                return $this->conn;
+            }
+            catch(PDOException $e){
+               die( "ERROR: ".$e->getMessage());
+            }
+        }
+    }
 ?>
